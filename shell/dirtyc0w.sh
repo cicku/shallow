7a462551e72aed2cbd7a8e68d23dc1c802a4bcdd
ᴄʜʀɪsᴛᴏᴘʜᴇʀ ᴍᴇɴɢ <im@dyi.ng>
2019-10-07 12:15:14 -0400

    Update logic

--- shell/dirtyc0w.sh
+++ shell/dirtyc0w.sh
@@ -5,22 +5,45 @@
 
 set -eu
 
+if [[ $EUID -eq 0 ]]; then
+    echo "This lab cannot be done under root"
+    exit 1
+fi
+
 trap "kill 0" SIGINT
+
 COW_FILE="/zzz"
 COW_FILE_ORIG="/zzz.bak"
 dirtyc0w="/home/seed/a.out"
 
-$dirtyc0w &
-COW_PID=$!
-while true
+echo "The original file content is:"
+cat $COW_FILE
+echo ""
+# Is running?
+i=0
+
+while :;
     do
-        if ! cmp -s $COW_FILE $COW_FILE_ORIG
+        if cmp -s $COW_FILE $COW_FILE_ORIG
             then
-        echo "SUCCESS... The file has been changed under read only mode."
-        kill -9 $COW_PID
-        exit 1
+            echo "COW attack in progress..."
+            echo ""
+            if [ $i == 0 ]; then 
+                $dirtyc0w &
+                COW_PID=$!
+                ((++i))
             else
-        echo "COW attack in progress..."
+                echo "Attack stalled!"
+                exit 1
+            fi
+        else
+            echo "SUCCESS... The file has been changed under read only mode."
+            echo "The new file content is:"
+            cat $COW_FILE
+            echo ""
+            kill -9 $COW_PID 2>/dev/null || echo "Attack cannot be performed, please check environment setup."
+            exit 1
+            # TODO: cleanup if needed
         fi
-    sleep 1
-done
+        sleep 1
+    done
